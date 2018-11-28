caac1977b16ab2db84cfaa3316e89a13c1bef6f2
MartinHan <hanhan145@163.com>
2018-11-28 23:58:28 +0800

    初始化项目

---
+++ app/src/main/java/com/martinhan/xposeddemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.martinhan.xposeddemo;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+
+import de.robv.android.xposed.XposedHelpers;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/martinhan/xposeddemo/XposedInit.java
@@ -0,0 +1,90 @@
+package com.martinhan.xposeddemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XposedInit implements IXposedHookLoadPackage {
+    private static String PACKAGE_NAME = "com.tencent.mm";
+    private static final String TAG = "MartinHan_xposed: ";
+
+    public void handleLoadPackage(LoadPackageParam lpparam) {
+        if (lpparam.packageName.equals(PACKAGE_NAME)) {
+            try {
+                wx(lpparam);
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+    }
+
+    private void wx(LoadPackageParam lpparam) {
+        XposedBridge.log("MartinHan_xposed: wx success");
+        try {
+            final ClassLoader classLoader = lpparam.classLoader;
+            XposedHelpers.findAndHookMethod("com.tencent.mm.ui.tools.MultiStageCitySelectUI", classLoader, "initView", new Object[]{new XC_MethodHook() {
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    XposedBridge.log("MartinHan_xposed: wx success MultiStageCitySelectUI beforeHookedMethod");
+                }
+
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    XposedBridge.log("MartinHan_xposed: wx success MultiStageCitySelectUI afterHookedMethod");
+                }
+            }});
+            XposedHelpers.findAndHookMethod("com.tencent.mm.ui.tools.MultiStageCitySelectUI", classLoader, "onCreate", new Object[]{Bundle.class, new XC_MethodHook() {
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    XposedBridge.log("MartinHan_xposed: wx success MultiStageCitySelectUI onCreate beforeHookedMethod");
+                }
+
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    XposedBridge.log("MartinHan_xposed: wx success MultiStageCitySelectUI  afterHookedMethod");
+                    Class mscsu = XposedHelpers.findClass("com.tencent.mm.ui.tools.MultiStageCitySelectUI", classLoader);
+                    Method[] methods = param.thisObject.getClass().getMethods();
+                    List<Method> myMethods = new ArrayList();
+                    for (Method item : methods) {
+                        if (item.getDeclaringClass().equals(mscsu)) {
+                            myMethods.add(item);
+                        }
+                    }
+                }
+            }});
+            XposedHelpers.findAndHookMethod("com.tencent.mm.ui.tools.MultiStageCitySelectUI", classLoader, "cJa", new Object[]{new XC_MethodHook() {
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    XposedBridge.log("MartinHan_xposed: wx success MultiStageCitySelectUI cJa beforeHookedMethod");
+                }
+
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    XposedBridge.log("MartinHan_xposed: wx success MultiStageCitySelectUI cJa  afterHookedMethod");
+                    Object wdnObj = XposedHelpers.findField(XposedHelpers.findClass("com.tencent.mm.ui.tools.MultiStageCitySelectUI", classLoader), "wdN").get(param.thisObject);
+                    Class regionClazz = XposedHelpers.findClass("com.tencent.mm.storage.RegionCodeDecoder$Region", classLoader);
+                    Field codeField = XposedHelpers.findField(regionClazz, "code");
+                    Field nameField = XposedHelpers.findField(regionClazz, "name");
+                    Object arrayHarryporrt = Array.get(wdnObj, 0);
+                    codeField.set(arrayHarryporrt, "哈利波特魔法学校1");
+                    nameField.set(arrayHarryporrt, "哈利波特魔法学校1");
+                    Array.set(wdnObj, 0, arrayHarryporrt);
+                }
+            }});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
